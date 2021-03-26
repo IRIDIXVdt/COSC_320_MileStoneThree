d4d8df30401f7d2f7ddd36f2d7b8bcf343f4b0db
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-03-26 18:40:54 +0800

    included the other costs
    public double monetaryCost;
    	public double waitTimeCost;
    	public double flightTimeCost;
    	public String path;
    	public double x1,x2,x3;//xi should be the coefficients for each costs

--- Node.java
+++ Node.java
@@ -2,15 +2,24 @@
 
 public class Node implements Comparable<Node>{
 	public int nodeIndex;
-	public int cost;
+	public double monetaryCost;
+	public double waitTimeCost;
+	public double flightTimeCost;
 	public String path;
+	public double x1,x2,x3;//xi should be the coefficients for each costs 
 	
-	public Node(int nodeIndex, int cost, String path) {
+	public Node(int nodeIndex, double monetaryCost, double waitTimeCost, double flightTimeCost, String path){
 		this.nodeIndex = nodeIndex;
-		this.cost = cost;
+		this.monetaryCost = monetaryCost;
+		this.waitTimeCost = waitTimeCost;
+		this.flightTimeCost = flightTimeCost;
 		this.path = path;
 	}
-	
+
+	public Node(int nodeIndex, double monetaryCost, String path) {
+		this(nodeIndex,monetaryCost,0,0,path);
+	}
+
 	public int compareTo(Node o) {
 		if(this.cost < o.cost) {
 			return -1;
---
+++ RandomMapGenerator.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+
+public class RandomMapGenerator {
+    LinkedList<Node>[] PathCostGraph;
+    LinkedList<Node>[] WaitTimeCostGraph;
+    LinkedList<Node>[] FlightTimeCostGraph;
+    
+}
