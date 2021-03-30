91a9d6a2b1329422fe5ec9a7799a2b09004945eb
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-03-30 15:29:25 +0800

    Calvin Qu's Update

--- USC.java
+++ USC.java
@@ -1,67 +1,76 @@
 
-
 import java.util.*;
 
-	 
-public class USC{
-	
+public class USC {
+
 	private PriorityQueue<Node> frontier = new PriorityQueue<>();
-	private int ver;//vertex numbers
-	private int[][] map;//flight map
+	private int ver;// vertex numbers
+	ArrayList<LinkedList<Node>> Map = new ArrayList<LinkedList<Node>>();
+	ArrayList<Double> distances; // Store cost of 1 node to other node.
+	HashSet<Node> settled; // HashSet to store the destination vertex in it.
 	
 	public USC() {
-		
+
 	}
 	
-	public USC(int ver, int[][] map) { //load map
+	// Constructor will initialize all variable like Map, distances, settled..
+	public USC(int ver, ArrayList<LinkedList<Node>> Map) { // load map
 		this.ver = ver;
-		this.map = map;
+		this.Map = Map;
+		this.distances = new ArrayList<Double>();
+		settled = new HashSet<Node>();
 	}
-
-	public List<Node> search(int initialState, int finalState){
+	
+	//search method will find path between initialState and finalState with minimum cost.
+	public List<Node> search(int initialState, int finalState) {
+		
 		List<Node> results = new ArrayList<>();
-		for(int i = 0; i < ver; i++) { //initial frontier
-			if(map[initialState][i]>0) {
-				Node x = new Node(i, map[initialState][i], initialState+"->"+i);
-				frontier.add(x);
-			}
+        int source = initialState; // source vertex
+        int destination = finalState; // destination vertex
+ 
+        for (int i = 0; i < ver; i++){
+        	distances.add(Double.MAX_VALUE); // initialize distances with MAXIMUM value
+        }
+        
+        distances.set(source, 0.0);
+        
+        for (Node n : Map.get(initialState)) { // initial frontier
+			Node x = new Node(n.getIndex(), n.getCost(), initialState + "->" + n.getIndex());
+			frontier.add(x);
+			distances.set(n.getIndex(), n.getCost());
 		}
-		System.out.println("initial frontier: "); //test
-		frontier.forEach((a)->System.out.println("(node,cost): (" + a.nodeIndex + "," + a.getCost() + ")")); //test
-		while(true) {
-			Node branch = frontier.poll(); 
-			if(branch.nodeIndex == finalState) {
-				results.add(branch);
-				Node newNode = frontier.peek();
-					while(newNode != null && newNode.nodeIndex == finalState && newNode.getCost() == branch.getCost()) {
-						results.add(frontier.poll());
-						newNode = frontier.peek();
-					}
+        while (!frontier.isEmpty()){ // check if priority queue is empty or not.
+            Node evaluationNode = frontier.poll();
+            if (evaluationNode.nodeIndex == destination) {
+				results.add(evaluationNode);
+				Node newNode = frontier.peek(); // take first node from priority queue and check for its adjacent
+				while (newNode != null && newNode.nodeIndex == destination && newNode.getCost() == evaluationNode.getCost()) {
+					results.add(frontier.poll());
+					newNode = frontier.peek();
+				}
 				return results;
 			}
-		System.out.println("branch to continue: ("+ branch.nodeIndex+","+branch.getCost() + ")" + "\n"); //test
-		updateFrontier(branch);	
-		}
+            settled.add(evaluationNode); // add visited node in settled HashSet.
+            updateFrontier(evaluationNode); // update priority queue with new branch.
+        }
+        return null;
 	}
-		
-		
+
 	public void updateFrontier(Node branch) {
-		for(int i = 0; i < ver; i++) {
-			if(map[branch.nodeIndex][i] > 0) {
-				Node x = new Node (i, branch.getCost() + map[branch.nodeIndex][i], branch.path + " -> " + i);
-				frontier.add(x);
-			}
+		for (Node n : Map.get(branch.getIndex())) {  // find new branch and add it to priority queue
+			Node x = new Node(n.getIndex(), branch.getCost() + n.getCost(),branch.path + " -> " + n.getIndex());
+			frontier.add(x);
 		}
-		System.out.println("New Branches: "); //test
-		frontier.forEach((a)->System.out.println("(node,cost:) ("+a.nodeIndex+","+a.getCost() + ")")); //test
 	}
-	
-	public void userInput(int s, int e) {
+
+	public void userInput(int s, int e) { 
+		// this method will take initialState and finalState
+		// call search method to get the minimum cost path.
+		// print the total cost of path.
 		List<Node> results = this.search(s, e);
-		for(Node result : results) {
-			System.out.println("Total Cost: " + result.getCost() + "\n" + "Total Path: "+ result.path);
+		for (Node result : results) {
+			System.out.println("Total Cost: " + result.getCost() + "\n" + "Total Path: " + result.path);
 		}
 	}
 
 }
-
---
+++ UniformCostSearch.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class UniformCostSearch {
+    LinkedList<Node>[] PathCostGraph;
+    int ver;//vertex
+    public UniformCostSearch(int ver, LinkedList<Node>[] PathCostGraph) { //load map
+		  this.ver = ver;
+		  this.PathCostGraph = PathCostGraph;
+	  }
+
+
+    
+
+}
